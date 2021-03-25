650887062c9dcf6f6de634aec06d00e0f0d1292b
annissafildzah <nisaafrp@gmail.com>
2021-03-25 14:38:48 +0700

    init with register user api

---
+++ handler/user.go
@@ -0,0 +1,43 @@
+package handler
+
+import (
+	"bwagolangvue/helper"
+	"bwagolangvue/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Failed to register account", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	newUser, err := h.userService.RegisterUser(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to register account", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(newUser, "tokentokentoken")
+
+	response := helper.APIResponse("Account has been created", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
